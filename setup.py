78863ccd3adac5182939076dc0f584e52492cb4c
Mathias Andersson <wraul@dbox.se>
2023-06-25 18:46:53 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="beets-albumflags",
+    version="0.1",
+    description="beets plugin to add flags to album names",
+    long_description="",
+    author="Mathias Andersson",
+    author_email="",
+    url="",
+    license="MIT",
+    platforms="ALL",
+    packages=["beetsplug"],
+    install_requires=["beets>=1.4.7"],
+)
