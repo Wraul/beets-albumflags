ff641df8dfcfab53dcee08a473a82e10400f0a36
Mathias Andersson <wraul@dbox.se>
2023-11-16 21:28:53 +0100

    Update version to 0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="beets-albumflags",
-    version="0.7",
+    version="0.8",
     description="beets plugin to add flags to album names",
     long_description="",
     author="Mathias Andersson",
